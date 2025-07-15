c228868a7ebacc830d1beb6bfaacb664df555776
rff ald <117867334+xiv3r@users.noreply.github.com>
2025-07-15 11:37:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 pkg install python python-pillow tesseract -y
 git clone https://github.com/xiv3r/lpb-bruteforce.git
-cd lpb-bruteforce
-pip install -r depends.txt
+pip install -r lpb-bruteforce/depends.txt
+python3 lpb-bruteforce/lpb.py -h
